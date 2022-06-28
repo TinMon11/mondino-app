612b05c269037a3f72ac27c3667f26f440be399c
TinMon11 <martindanielmondino@gmail.com>
2022-06-28 14:52:51 -0300

    Se agrega CartWidget y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./NavBar.js";
+import ItemListContainer from "./ItemListContainer";
+
 
 function App() {
   return (
-    <div class="navbar bg-base-300 rounded-md w-4/5 mx-auto	">
-      <div class="flex-1">
-        <a class="btn normal-case text-xl">TodoMedias</a>
-      </div>
-      <div class="flex-none">
-        <ul class="menu menu-horizontal p-5">
-          <li>
-            <a>Hombre</a>
-          </li>
-          <li>
-            <a>Mujer</a>
-          </li>
-          <li>
-            <a>Niños</a>
-          </li>
-        </ul>
-        <div class="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            class="input input-bordered"
-          />
-        </div>
-      </div>
+    <div className="App">
+      <NavBar/>
+      <ItemListContainer greeting={"APRENDIENDO REACT"}/>
     </div>
-  );
-}
-
+  )}
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import CartWidget from "./CartWidget";
+
+const NavBar = () => {
+    return (
+        <div class="navbar bg-base-300 rounded-md w-4/5 mx-auto	">
+          <div class="flex-1">
+            <a class="btn normal-case text-xl">TodoMedias</a>
+          </div>
+          <div class="flex-none">
+            <ul class="menu menu-horizontal p-5">
+              <li><a>Hombre</a></li>
+              <li><a>Mujer</a></li>
+              <li><a>Niños</a></li>
+            </ul>
+            <div class="form-control">
+              <input
+                type="text"
+                placeholder="Search"
+                class="input input-bordered"
+              />
+            </div>
+            <CartWidget/>
+          </div>
+    </div>
+      ) }
+
+export default NavBar;
