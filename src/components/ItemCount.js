182957df3728dea2a7a10aa04193c658991a56d0
TinMon11 <martindanielmondino@gmail.com>
2022-07-04 16:00:44 -0300

    Se corrige className y variable initial en ItemCount

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-function ItemCount({stock, inicial, onAdd}){
-    const [count, setCount] = useState(inicial);
+function ItemCount({stock, initial, onAdd}){
+    const [count, setCount] = useState(initial);
 
     const incrementar = () => {
         if (count < stock) {setCount(count+1)}
@@ -18,18 +18,18 @@ function ItemCount({stock, inicial, onAdd}){
 
     return (
 
-<div class="card w-64 bg-base-100 shadow-xl">
+<div className="card w-64 bg-base-100 shadow-xl">
   <figure><img src="https://http2.mlstatic.com/D_NQ_NP_862700-MLA47307297782_082021-W.jpg" alt="Medias" /></figure>
-  <div class="card-body">
-    <h2 class="card-title justify-center">Medias BlackOut</h2>
+  <div className="card-body">
+    <h2 className="card-title justify-center">Medias BlackOut</h2>
     <p>¡Nuestro modelo favorito!</p>
-    <div class="card-actions justify-around items-center">
-      <button class="btn btn-circle bg-primary btn-sm" onClick={() => incrementar()}>+</button>
-      <p class="font-bold text-2xl">{count}</p>
-      <button class="btn btn-circle bg-neutral btn-sm" onClick={() => decrementar()}>-</button>
+    <div className="card-actions justify-around items-center">
+      <button className="btn btn-circle bg-primary btn-sm" onClick={() => incrementar()}>+</button>
+      <p className="font-bold text-2xl">{count}</p>
+      <button className="btn btn-circle bg-neutral btn-sm" onClick={() => decrementar()}>-</button>
     </div>
-    <div class="card-actions justify-center">
-      <button class="btn btn-primary bg-primary w-9/12" onClick={() => onAdd(count)}>Agregar Al Carrito</button>
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary bg-primary w-9/12" onClick={() => onAdd(count)}>Agregar Al Carrito</button>
     </div>
   </div>
 </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,21 @@ import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
-        <div class="navbar bg-base-300 rounded-md w-4/5 mx-auto	">
-          <div class="flex-1">
-            <a class="btn normal-case text-xl">TodoMedias</a>
+        <div className="navbar bg-base-300 rounded-md w-4/5 mx-auto	">
+          <div className="flex-1">
+            <a className="btn normal-case text-xl">TodoMedias</a>
           </div>
-          <div class="flex-none">
-            <ul class="menu menu-horizontal p-5">
+          <div className="flex-none">
+            <ul className="menu menu-horizontal p-5">
               <li><a>Hombre</a></li>
               <li><a>Mujer</a></li>
               <li><a>Niños</a></li>
             </ul>
-            <div class="form-control">
+            <div className="form-control">
               <input
                 type="text"
                 placeholder="Search"
-                class="input input-bordered"
+                className="input input-bordered"
               />
             </div>
             <CartWidget/>
