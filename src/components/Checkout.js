cf739c355c17038d61e679659f218715375e9659
TinMon11 <tincho_mondino@hotmail.com>
2022-08-04 04:27:24 -0300

    Se agrega SweetAlert. Se completa el uso del Form para generar orden

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,22 +3,35 @@ import { useContext } from "react";
 import { Link } from 'react-router-dom';
 import CartContext from '../context/CartContext';
 import Form from './Form';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, Timestamp } from 'firebase/firestore';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
+function Checkout() {
 
-function Cart() {
+  const { cart, totalItems, totalPrice, clearAll } = useContext(CartContext)
+  const MySwal = withReactContent(Swal)
+  const [userData, setUserData] = useState({name: '', phone: '', email: '', repEmail: ''});
 
-  const { cart, totalItems, totalPrice } = useContext(CartContext)
-  const [idOrden, setIdOrden] = useState("")
+  const notif = (id) =>  MySwal.fire({
+    title: <strong>Order Placed!</strong>,
+    html: <i>Your order Id is {id}</i>,
+    icon: 'success'
+     })  
 
+  const handleChange = (event) => {
+    setUserData({
+        ...userData,
+        [event.target.name]: event.target.value
+    });
+}
 
-  const placeOrder = () => {
-    const fecha = new Date();
-    const fechaOrden = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
-
+  const placeOrder = (e) => {
+    e.preventDefault()
+    const fechaOrden = Timestamp.fromDate(new Date())
     const order = {
-      buyer: { name: "Martin", phone: "+542954328298", email: "tincho_mondino@hotmail.com" },
+      buyer: userData,
       cart,
       totalPrice,
       fechaOrden
@@ -26,22 +39,19 @@ function Cart() {
 
     const db = getFirestore();
     const ordersCollection = collection(db, "orders")
-    addDoc(ordersCollection, order).then(doc => setIdOrden(doc.id));
-
-  }
+    addDoc(ordersCollection, order).then((doc) => {
+      notif(doc.id)
+      })
+    .catch(err => console.log(err))
+    .finally(()=> {
+      clearAll();
+      setUserData({name: "", phone: "", email: "", repEmail: ""})}
+      );
+}
 
   return (
 
     <section>
-
-      {idOrden !== "" &&
-        <div className="mt-10 alert alert-success shadow-lg w-8/12 m-auto">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>Your order has been placed. Order ID : {idOrden} </span>
-          </div>
-        </div>
-      }
 
       {cart.length > 0 &&
         <div className="flex mx-auto w-8/12 mt-10 mb-10 font-bold">
@@ -49,17 +59,19 @@ function Cart() {
           <div className="divider divider-horizontal"></div>
           <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center shadow-xl">TOTAL ${totalPrice}</div>
         </div>}
+
       <div className='mt-10 text-center'>
-
-        <Form/>
-
-
+      
+      {cart.length > 0 && 
+      <div>
+      <Form handleChange={handleChange} placeOrder={placeOrder} userData={userData}/>
+      </div>
+      } 
         <div>
-          <button className="btn btn-primary w-48 mr-10" onClick={placeOrder}>PLACE ORDER</button>
+        
           <Link to="../">
             <button className="btn btn-primary btn-block w-48">BACK TO STORE</button>
           </Link>
-
         </div>
       </div>
     </section>
@@ -67,4 +79,4 @@ function Cart() {
   )
 }
 
-export default Cart;
+export default Checkout;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,64 @@
 import React from 'react'
 
-const Form = () => {
+const Form = ({handleChange, userData, placeOrder}) => {
     return (
-
-        <div>
-
-            <form className='mt-6 w-8/12 m-auto'>
+            <form onChange={handleChange} className='mt-6 w-8/12 m-auto'>
                 <div className="relative z-0 mb-6 w-full group">
-                    <input type="email" name="floating_email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""></input>
-                        <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder="Enter your email"
+                        defaultValue={userData.name}>
+                    </input>
                 </div>
 
                 <div className="relative z-0 mb-6 w-full group">
-                    <input type="email" name="floating_email" id="repeat_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""></input>
-                        <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Repeat Email</label>
+                    <input
+                        type="email"
+                        name="repEmail"
+                        id="repEmail"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder="Repeat your email"
+                        defaultValue={userData.repEmail}>
+                    </input>
                 </div>
-                
+
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="floating_first_name" id="first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""></input>
-                            <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            placeholder="Enter your name"
+                            defaultValue={userData.name}>
+                        </input>
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="floating_last_name" id="last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""></input>
-                            <label for="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                        <input
+                            type="text"
+                            name="phone"
+                            id="phone"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            placeholder="Phone Number "
+                            defaultValue={userData.phone}>
+                        </input>
                     </div>
                 </div>
-                
 
+                <button
+                    onClick={placeOrder}
+                    disabled={!(userData.name !== ''
+                        && userData.phone !== ''
+                        && userData.email === userData.repEmail
+                        && userData.phone.length > 5
+                    )}
+                    className="btn btn-primary btn-block w-48 mb-5">
+                   Place Order
+                </button>
             </form>
-
-        </div>
     )
 }
 
